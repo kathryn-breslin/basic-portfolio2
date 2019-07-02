3d77742acea9e9074061227ecbac7cf3d55dee67
kathryn-breslin <kathryn.l.breslin@gmail.com>
2019-07-02 19:42:38 -0400

    working on comparing guesses and letters in word

--- js/game.js
+++ js/game.js
@@ -6,6 +6,7 @@
 var wins;
 var losses;
 var userGuesses = [];
+var filteredUserGuesses;
 var blanksForRandomWord = [];
 var randomMoon = [];
 var guessesRemaining;
@@ -28,32 +29,59 @@ function startGame() {
 
     randomMoon = moonPhases[Math.floor(Math.random() * moonPhases.length)];
     console.log(randomMoon);
+    createUnderscores();
+}
 
-    //Show random word/moon on the page
+function createUnderscores() {
     for (var i = 0; i < randomMoon.length; i++) {
         // console.log(randomMoon.charAt(i));
-        blanksForRandomWord.push(' __ ');
+        blanksForRandomWord.push(" __ ");
     }
     document.getElementById("moonUnderscores").innerHTML = blanksForRandomWord.join(" ");
+    checkUserInput();
+}
 
+function checkUserInput() {
     document.onkeyup = function (event) {
         var userInput = event.key.toLowerCase();
         userGuesses.push(userInput);
-        console.log(userGuesses)
-        document.getElementById("guessesSoFar").textContent = userGuesses;
-        
+        screenDuplicates();
+        // console.log(userGuesses)
         //if statement to replace underscore with user guess, if correct
         if (randomMoon.indexOf(userInput) > -1) {
             for (var j = 0; j < randomMoon.length; j++) {
                 if (userInput === randomMoon[j]) {
                     blanksForRandomWord[j] = userInput;
-                    document.getElementById("moonUnderscores").innerHTML = blanksForRandomWord.join("");
+                    document.getElementById("moonUnderscores").innerHTML = blanksForRandomWord.join(" ");
                 }
             }
         }
     }
-
 }
+
+function screenDuplicates() {
+    filteredUserGuesses = userGuesses.filter(function (item, position) {
+        return (userGuesses.indexOf(item) === position)
+    });
+    document.getElementById("guessesSoFar").textContent = filteredUserGuesses;
+}
+
+function compareFilteredToRandomWord (filtered, random) {
+    var finalArray = [];
+
+    for (var i = 0; i < filtered.length; i++) {
+        if (random.indexOf(filtered[i]) > -1) {
+            finalArray.push(filtered[i]);
+        }
+        return finalArray;
+    }    // document.getElementById("guessesSoFar").textContent = filteredUserGuesses;
+    console.log("This is the final array: " + finalArray)
+}
+
 startGame();
 
-//Getting the user keypress
+//Letter should not be pushed into "Guesses so Far" array if it is found in the random word
+//Guesses remaining decreases with each guess only if incorrect
+//if "Guesses Remaining" runs out, "Losses" increases
+//if word is guessed correctly, Wins increases
+//Restart the game after the round is either won or lost
